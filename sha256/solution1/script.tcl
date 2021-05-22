ba5c28967d56c9fae90430c0fdcb1b0155f28c9a
Ransara Wijitharathna <ransarawijitha@gmail.com>
2021-05-22 21:51:02 +0530

    Initial commit

---
+++ sha256/solution1/script.tcl
@@ -0,0 +1,17 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project sha256
+add_files sha256.cpp
+add_files sha256.h
+add_files -tb shatest.cpp -cflags "-DHW_COSIM"
+open_solution "solution1" -flow_target vivado
+set_part {xcvu11p-flga2577-1-e}
+create_clock -period 10 -name default
+#source "./sha256/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -format ip_catalog
